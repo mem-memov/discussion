ae6ab06efcd14f90383dc674864981b032c08f01
Александр Новиков <anovikov@tsum.ru>
2020-11-27 14:35:27 +0300

    question

--- Interpreter/Merging/Question.hs
+++ Interpreter/Merging/Question.hs
@@ -4,27 +4,27 @@ import Language
 
 ask :: Answer -> Question -> Question
 ask NoAnswer correctQuestion@(CorrectQuestion _ _ _ _) = correctQuestion
--- askQuestion
---     questionedAnswer@(CorrectAnswer 
---         (AnsweredQuestion answeredQuestion) 
---         (PreviousAnswer previousAnswer) 
---         answerString 
---         (QuestionsToAnswer questionsToAnswer)
---     )
---     question'@(CorrectQuestion 
---         (QuestionedAnswer NoAnswer) 
---         (PreviousQuestion previousQuestion) 
---         questionString 
---         (AnswersToQuestion answersToQuestion)
---     )
---     = CorrectQuestion
---         (QuestionedAnswer questionedAnswer) 
---         (PreviousQuestion
---             (case questionsToAnswer of 
---                 [] -> NoQuestion
---                 previousQuestion : _ -> previousQuestion
---             )
---         ) 
---         questionString 
---         (AnswersToQuestion answersToQuestion)
+ask
+    questionedAnswer@(CorrectAnswer 
+        (AnsweredQuestions answeredQuestions) 
+        (PreviousAnswer previousAnswer) 
+        answerString 
+        (QuestionsToAnswer questionsToAnswer)
+    )
+    question@(CorrectQuestion 
+        (QuestionedAnswers questionedAnswers) 
+        (PreviousQuestion previousQuestion) 
+        questionString 
+        (AnswersToQuestion answersToQuestion)
+    )
+    = CorrectQuestion
+        (QuestionedAnswers (questionedAnswer : questionedAnswers)) 
+        (PreviousQuestion
+            (case questionsToAnswer of 
+                [] -> NoQuestion
+                previousQuestion : _ -> previousQuestion
+            )
+        ) 
+        questionString 
+        (AnswersToQuestion answersToQuestion)
 
